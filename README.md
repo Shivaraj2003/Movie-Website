42e3372446b7d2d0477bb5994d8e654441f8373c
Shivaraj Shetty <shivarajshetty635@gmail.com>
2024-12-16 15:40:30 +0530

    Simple anime search website by using React

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Movie-Website
+# Anime Search Website🚀
 
 ## React JS Project using Vite
+
+Understood the basics of react by building a mini project that uses Jikan Api to get the anime database.
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-
-import './App.css'
-import MovieCard from './components/MovieCard'
+import { useEffect, useState } from "react";
+import AnimeCard from "./AnimeCard";
 
 function App() {
-return (
+  const [animes, setAnimes] = useState([]);
+  const [term, setTerm] = useState("naruto");
+
+  const getAnime = async (name) => {
+    const response = await fetch(`https://api.jikan.moe/v4/anime?q=${name}`);
+    const data = await response.json();
+    console.log(data.data);
+    setAnimes(data.data);
+  };
+
+  useEffect(() => {
+    getAnime("naruto");
+  }, []);
+
+  return (
     <>
-      <MovieCard movie={{title:'KGF', release_date:2024}}/>
-      
+      <h1>Anime Search</h1>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Search for anime e.g Naruto"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+        <button onClick={() => getAnime(term)}>Search</button>
+      </div>
+      <div className="container">
+        {animes?.length > 0 ? (
+          animes.map((anime, index) => <AnimeCard key={index} anime={anime} />)
+        ) : (
+          <div>Not Found</div>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
